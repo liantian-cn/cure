6acad6b413e23caa3c808f863b4c1ecd26c740b7
liantian <liantian.me+code@gmail.com>
2025-08-30 11:13:42 +0800

    Remove cureItem and cureAura logic, add migration notes
    The logic in cureItem.lua and most of cureAura.lua has been removed and replaced with comments indicating migration to https://github.com/liantian-cn/RotationLib. The cure.toc file has been updated to comment out cureItem.lua and add a migration note. This change reflects the transition of functionality to the new library.

--- cureItem.lua
+++ cureItem.lua
@@ -1,15 +1,2 @@
---- 检查指定装备槽位的物品是否可以使用
---- @param slot number 装备槽位ID
---- @return boolean 如果物品可以使用返回true，否则返回false
-Cure.Item.slotUsable = function(slot)
-    local itemId = GetInventoryItemID("player", slot)
-    local usable, noMana = C_Item.IsUsableItem(itemId)
-    if (not usable) or noMana then
-        return false
-    end
-    local _, duration, enable = C_Container.GetItemCooldown(itemId)
-    if (enable ~= 1) or (duration ~= 0) then
-        return false
-    end
-    return true
-end
+
+-- 已迁移到 https://github.com/liantian-cn/RotationLib
